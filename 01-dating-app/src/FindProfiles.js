0ee168276a5d27e79f47f0f97970d81a3df04993
roowahoo <tohyinru95@gmail.com>
2021-03-17 06:47:05 +0000

    edited search bar css

--- 01-dating-app/src/FindProfiles.js
+++ 01-dating-app/src/FindProfiles.js
@@ -72,118 +72,80 @@ export default class FindProfiles extends React.Component {
     render() {
         return (
             <React.Fragment>
-                {/* <ButtonDropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
-                    <DropdownToggle caret>
-                        Gender
-                    </DropdownToggle>
-                    <DropdownMenu>
-                        <DropdownItem value='male' name='gender' onClick={this.searchProfile}>Male</DropdownItem>
-                        <DropdownItem value='female' name='gender' onClick={this.searchProfile}>Female</DropdownItem>
-                    </DropdownMenu>
-                </ButtonDropdown> */}
-                <div style={{ display: this.state.display ? 'block' : 'none' }}>
+                <div id='search'>
+                    <div className='row pb-2' >
+                        <div className='col-3'>
+                            <Label for="exampleSelect">Gender</Label>
+                            <Input onChange={this.updateFormFields} type="select" name="gender" id="exampleSelect">
+                                <option value='' name=''>--Select--</option>
+                                <option value='male' name='gender'>Male</option>
+                                <option value='female' name='gender'>Female</option>
+                            </Input>
+                        </div>
+                        <div className='col-3'>
 
-                    <Label for="exampleSelect">Gender</Label>
-                    <Input onChange={this.updateFormFields} type="select" name="gender" id="exampleSelect">
-                        <option value='' name=''>--Select--</option>
-                        <option value='male' name='gender'>Male</option>
-                        <option value='female' name='gender'>Female</option>
-                    </Input>
+                            <Label for="exampleSelect">Age Range</Label>
+                            <Input onChange={this.updateFormFields} type="select" name="age" id="exampleSelect">
+                                <option value='' name=''>--Select--</option>
+                                <option value='20' name='age'>20s</option>
+                                <option value='30' name='age'>30s</option>
+                            </Input>
+                        </div>
 
-                    <Label for="exampleSelect">Age Range</Label>
-                    <Input onChange={this.updateFormFields} type="select" name="age" id="exampleSelect">
-                        <option value='' name=''>--Select--</option>
-                        <option value='20' name='age'>20s</option>
-                        <option value='30' name='age'>30s</option>
-                    </Input>
+                    </div>
 
-                    <div className=''>
-                        <div className='form-check'>
-                            <input className='form-check-input' type='checkbox' name='interests' value='sports' onChange={this.updateInterests} />
-                            <label className='form-check-label'>Sports</label>
+                    <Label>Interests</Label>
+                    <div className='d-flex'>
+
+                        <div className='mx-5'>
+                            <Label check>
+                                <Input type='checkbox' name='interests' value='sports' onChange={this.updateInterests} />Sports</Label>
                         </div>
-                        <div className='form-check'>
-                            <input className='form-check-input' type='checkbox' name='interests' value='arts' onChange={this.updateInterests} />
-                            <label className='form-check-label'>Arts</label>
+
+                        <div className='mx-5'>
+                            <Label check>
+                                <Input type='checkbox' name='interests' value='arts' onChange={this.updateInterests} />Arts</Label>
                         </div>
-                        <div className='form-check'>
-                            <input className='form-check-input' type='checkbox' name='interests' value='food' onChange={this.updateInterests} />
-                            <label className='form-check-label'>Food</label>
+
+                        <div className='mx-5'>
+                            <Label check>
+                                <Input type='checkbox' name='interests' value='food' onChange={this.updateInterests} />Food</Label>
                         </div>
-                        <div className='form-check'>
-                            <input className='form-check-input' type='checkbox' name='interests' value='travel' onChange={this.updateInterests} />
-                            <label className='form-check-label'>Travel</label>
+
+                        <div className='mx-5'>
+                            <Label check >
+                                <Input type='checkbox' name='interests' value='travel' onChange={this.updateInterests} />Travel</Label>
                         </div>
-                        <div className='form-check'>
-                            <input className='form-check-input' type='checkbox' name='interests' value='education' onChange={this.updateInterests} />
-                            <label className='form-check-label'>Education</label>
+
+                        <div className='mx-5'>
+                            <Label check>
+                                <Input type='checkbox' name='interests' value='education' onChange={this.updateInterests} />Education</Label>
                         </div>
                     </div>
 
-                    <button className='btn pinkBtn col-2' onClick={this.searchProfile}>Search</button>
-
-
-
-                    {/* <div className='row' id='search'>
-                        <div className='col-3'>
-                            <button className='btn btn-secondary' onClick={this.byGender}>Gender</button>
-                            <button className='btn btn-secondary m-1' style={{ display: this.state.byGender ? 'block' : 'none' }} value='male' name='gender' onClick={this.updateFormFields}>Male</button>
-                            <button className='btn btn-secondary m-1' style={{ display: this.state.byGender ? 'block' : 'none' }} value='female' name='gender' onClick={this.updateFormFields}>Female</button>
-                        </div>
-
-                        <div className='col-3'>
-                            <button className='btn btn-secondary' onClick={this.byAge}>Age</button>
-                            <button className='btn btn-secondary' style={{ display: this.state.byAge ? 'block' : 'none' }} value='20' name='age' onClick={this.updateFormFields}>20s</button>
-                            <button className='btn btn-secondary' style={{ display: this.state.byAge ? 'block' : 'none' }} value='30' name='age' onClick={this.updateFormFields}>30s</button>
-                        </div>
-
-                        <div className='col-3'>
-                            <button className='btn btn-secondary' onClick={this.byInterest}>Interests</button>
-                            <div style={{ display: this.state.byInterest ? 'block' : 'none' }}>
-                                <div className='form-check'>
-                                    <input className='form-check-input' type='checkbox' name='interests' value='sports' onChange={this.updateInterests} />
-                                    <label className='form-check-label'>Sports</label>
-                                </div>
-                                <div className='form-check'>
-                                    <input className='form-check-input' type='checkbox' name='interests' value='arts' onChange={this.updateInterests} />
-                                    <label className='form-check-label'>Arts</label>
-                                </div>
-                                <div className='form-check'>
-                                    <input className='form-check-input' type='checkbox' name='interests' value='food' onChange={this.updateInterests} />
-                                    <label className='form-check-label'>Food</label>
-                                </div>
-                                <div className='form-check'>
-                                    <input className='form-check-input' type='checkbox' name='interests' value='travel' onChange={this.updateInterests} />
-                                    <label className='form-check-label'>Travel</label>
-                                </div>
-                                <div className='form-check'>
-                                    <input className='form-check-input' type='checkbox' name='interests' value='education' onChange={this.updateInterests} />
-                                    <label className='form-check-label'>Education</label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <button className='btn btn-primary col-3' onClick={this.searchProfile}>Search</button>
-
-                    </div> */}
-
-
-                    <div id='profiles'>
-                        {this.renderProfiles()}
+                    <div className='d-flex justify-content-end'>
+                            <button className='btn pinkBtn' onClick={this.searchProfile}>Search</button>
                     </div>
 
-                    <div id='popup' style={{ display: this.state.validate ? 'block' : 'none' }}>
-                        <label className='form-label'>Username:</label>
-                        <input className='form-text' type='text' name='username' value={this.state.username} onChange={this.updateFormFields}></input>
-                        <button className='btn btn-primary' onClick={this.validate}>Submit</button>
-                    </div>
                 </div>
+
+
+
+                <div id='profiles'>
+                    {this.renderProfiles()}
+                </div>
+
+                <div id='popup' style={{ display: this.state.validate ? 'block' : 'none' }}>
+                    <label className='form-label'>Username:</label>
+                    <input className='form-text' type='text' name='username' value={this.state.username} onChange={this.updateFormFields}></input>
+                    <button className='btn btn-primary' onClick={this.validate}>Submit</button>
+                </div>
+
 
                 <div style={{ display: this.state.conversations ? 'block' : 'none' }}>
                     {/* <Conversations conversationId={this.state.conversationId} message={this.state.message} /> */}
                     <input type='text' name='message' value={this.state.message} onChange={this.updateFormFields}></input>
                     <button className='btn btn-primary' onClick={this.send}>Send</button>
-
                 </div>
 
             </React.Fragment>
@@ -246,6 +208,7 @@ export default class FindProfiles extends React.Component {
             age: this.state.age
 
         }
+        console.log(searchValue)
         // if (this.state.interests.length === 0) {
         // console.log(searchValue)
         // let response = await axios.post(baseURL + '/searchbygender', searchValue)
